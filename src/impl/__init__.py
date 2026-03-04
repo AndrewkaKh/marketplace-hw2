275d456dcda4ec8cd8f0cee6a123f92f6de5e45c
Andrew <aikhramov@edu.hse.ru>
2026-03-05 00:07:09 +0300

    del src/impl

--- src/impl/__init__.py
+++
@@ -1,4 +0,0 @@
-from impl import auth as _auth  # noqa: F401
-from impl import products as _products  # noqa: F401
-from impl import orders as _orders  # noqa: F401
-from impl import promo_codes as _promo  # noqa: F401
